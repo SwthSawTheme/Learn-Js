5ef372a2cee559a1a884f226275affc0dccc03b4
SwthSawTheme <sawtheme1@gmail.com>
2025-01-24 22:07:08 -0300

    Manipulando strings

---
+++ js_Basico_ao_Avancado/script/strings.js
@@ -0,0 +1,13 @@
+// Variaveis do tipo String
+console.log("Jogos mortais III")
+console.log("Cemitério Maldito")
+console.log("Esc\"ape")
+
+
+var string = "texto"
+console.log(string.length)
+
+// Percorrendo uma string
+for (i=0; i < string.length; i++){
+    console.log(string[i])
+}
