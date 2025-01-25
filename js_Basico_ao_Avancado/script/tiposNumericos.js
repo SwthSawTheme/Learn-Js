1f7eba37f71b2abcdaf51f6665e5b47038055641
SwthSawTheme <sawtheme1@gmail.com>
2025-01-24 21:58:07 -0300

    Usando tipos númericos em Js

---
+++ js_Basico_ao_Avancado/script/tiposNumericos.js
@@ -0,0 +1,11 @@
+// Trabalhando com Tipos numéricos
+console.log(1.1)
+console.log(1.2)
+console.log(0.12)
+console.log(1,2)
+// Numero de pi usando lib Math
+console.log(Math.PI)
+// Raiz quadrada usando Math
+console.log(Math.sqrt(16))
+// Numero de Euler
+console.log(Math.E)
