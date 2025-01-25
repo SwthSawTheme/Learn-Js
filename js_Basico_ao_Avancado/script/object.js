39384c6a50a6bf0fcef97b6d014562402400cec8
SwthSawTheme <sawtheme1@gmail.com>
2025-01-24 22:24:09 -0300

    Objetos em javascripts

---
+++ js_Basico_ao_Avancado/script/object.js
@@ -0,0 +1,14 @@
+// Objetos
+
+console.log({ propriedade: "valor"})
+
+console.log({
+    nome: "Saw",
+    idade: 25,
+    altura: 1.89,
+    teste: {
+        nome: "Magic",
+        power: "Fire",
+        damage: 678.5
+    }
+})
