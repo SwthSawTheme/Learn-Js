80666983c15821c15a06475d65fc9a959c460862
SwthSawTheme <sawtheme1@gmail.com>
2025-01-24 21:51:07 -0300

    Variaveis boleanas

---
+++ js_Basico_ao_Avancado/boolean.js
@@ -0,0 +1,13 @@
+console.log(true)   // Verdadeiro
+console.log(false)  // Falso
+
+console.log(0 == false)
+// exemplo
+
+var cliente = false
+
+if (cliente == true){
+    console.log("Acesso autorizado!")
+} else {
+    console.log("Acesso negado!")
+}
