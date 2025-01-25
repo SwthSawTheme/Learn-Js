c102caf337ec8f618b98d2c7930133e1f5be8558
SwthSawTheme <sawtheme1@gmail.com>
2025-01-24 21:33:22 -0300

    Tipagem

---
+++ js_Basico_ao_Avancado/Tipagem.js
@@ -0,0 +1,11 @@
+var bool = true
+var number = 20
+var string = "texto"
+var float = 1.4
+var conta = "2" + 2
+
+console.log(typeof bool)
+console.log(typeof number)
+console.log(typeof string)
+console.log(typeof float)
+console.log(typeof conta)
