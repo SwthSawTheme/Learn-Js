23a6f2d92f4699853744bd817dd43d73a9d01e5d
SwthSawTheme <sawtheme1@gmail.com>
2025-01-27 21:31:10 -0300

    Operadores aritméticos

---
+++ js_Basico_ao_Avancado/script/operadores.js
@@ -0,0 +1,14 @@
+
+// Os operadores obedecem as ordens de precedência aritmética
+// () -> (* e /) -> (+ e -) 
+
+let n1, n2, n3, n4
+
+n1 = 5.0
+n2 = 4.0
+n3 = 10
+n4 = 2.2
+
+var media = (n1 + n2 + n3 + n4) / 4
+
+console.log(`A média aritmética é de ${media}`)
