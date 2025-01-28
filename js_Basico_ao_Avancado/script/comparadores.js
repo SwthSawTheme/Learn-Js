a7553e839756bac612442f8092b98b898cb09a66
SwthSawTheme <sawtheme1@gmail.com>
2025-01-27 21:41:10 -0300

    Comparadores

---
+++ js_Basico_ao_Avancado/script/comparadores.js
@@ -0,0 +1,9 @@
+
+// Comparadores em Js
+
+var numero = 1 > 2 // 1 maior que 2
+var numero1 = 1 < 2 // 1 menor que 2
+var numero2 = 1 == 1 // 1 igual a 1
+var numero3 = 3 != 2 // 3 diferente de 2
+
+console.log(numero,numero1,numero2,numero3)
