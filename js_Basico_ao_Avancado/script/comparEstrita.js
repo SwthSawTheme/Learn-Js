ea906a69b6d45ab2136bd2368113517b840dbc52
SwthSawTheme <sawtheme1@gmail.com>
2025-01-27 21:44:55 -0300

    Comparação estrita (idêntica)

---
+++ js_Basico_ao_Avancado/script/comparEstrita.js
@@ -0,0 +1,17 @@
+
+// comparação normal em Js
+
+var teste = "1" == 1
+var teste1 = null == undefined
+
+console.log(teste)
+console.log(teste1)
+
+// comparação estrita ou "identica"
+var teste2 = "1" === 1
+var teste3 = null === undefined
+var teste4 = null !== undefined
+
+console.log(teste2)
+console.log(teste3)
+console.log(teste4)
