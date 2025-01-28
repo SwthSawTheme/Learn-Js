1f15805942c75a0af3dc1d753153a2214b2c8bb6
SwthSawTheme <sawtheme1@gmail.com>
2025-01-27 21:20:58 -0300

    Declaração de variavels undefined e assignment

---
+++ js_Basico_ao_Avancado/script/declarationAssigm.js
@@ -0,0 +1,12 @@
+
+// Variavel iniciada "undefined"
+let test
+
+// Variavel iniciada "undefined"
+var number
+
+// Constante é obrigatório a atribuição de valor
+const userful = 1.0
+
+// Variavel declarada "Assignment"
+var name = "Saw"
