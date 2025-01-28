56cfdc1f9b6c729c7d3021970fca2476f6c224b6
SwthSawTheme <sawtheme1@gmail.com>
2025-01-27 22:01:33 -0300

    O que o javascript considera verdadeiro e falso

---
+++ js_Basico_ao_Avancado/script/falsyAndTruthly.js
@@ -0,0 +1,8 @@
+// Falsy and Truthly
+
+console.log( 0 ? "Verdadeiro" : "False")
+console.log(-0 ? "Verdadeiro" : "Falso")
+console.log(0n ? "Verdadeiro" : "Falso")
+console.log(null ? "Verdadeiro" : "Falso")
+console.log(undefined ? "Verdadeiro" : "Falso")
+console.log(NaN ? "Verdadeiro" : "Falso")
