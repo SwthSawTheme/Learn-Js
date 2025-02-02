65b2d392764a379964058408be3a83d8c3d3bc86
SwthSawTheme <sawtheme1@gmail.com>
2025-02-02 14:25:25 -0300

    Usando if else para fluxo de um programa

---
+++ js_Basico_ao_Avancado/script/elseIf.js
@@ -0,0 +1,10 @@
+
+const valor = 11
+
+if ( 1 >= valor && valor <= 5) {
+    console.log("Está dentro do range")
+} else if ( valor >= 6 && valor <= 10) {
+    console.log("Ainda está dentro do range")
+} else {
+    console.log("Está fora do range")
+}
