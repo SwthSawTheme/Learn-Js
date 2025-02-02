206c85b62f18f79e8c523573cc6db947d8af68fc
SwthSawTheme <sawtheme1@gmail.com>
2025-02-02 14:56:31 -0300

    For of em Js

--- js_Basico_ao_Avancado/script/forLoop.js
+++ js_Basico_ao_Avancado/script/forLoop.js
@@ -1,6 +1,10 @@
 
-
 var lista = ["Uno","Duno","caind","Suin","duli","Min"]
-for (var i=0; i < 100; i++) {
-    console.log(`Então é ${lista[i]}`)
+
+//for (var i=0; i < 100; i++) {
+//    console.log(`Então é ${lista[i]}`)
+//}
+
+for (const nome of lista) {
+    console.log(nome)
 }
