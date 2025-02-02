0f2ff61942cef7aaa379135a61770443c205ff84
SwthSawTheme <sawtheme1@gmail.com>
2025-02-02 14:45:41 -0300

    Uso do while

---
+++ js_Basico_ao_Avancado/script/while.js
@@ -0,0 +1,11 @@
+
+// Enquanto a condição for verdadeiro, execute
+
+var contador = 0
+
+while (contador < 10) {
+    console.log(`Executando... ${contador + 1}`)
+    contador ++
+}
+
+console.log("Fim da execução!")
