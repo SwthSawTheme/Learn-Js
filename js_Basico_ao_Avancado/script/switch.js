739f8331eb4440f856d3062d272e15004a3773ce
SwthSawTheme <sawtheme1@gmail.com>
2025-02-02 14:29:32 -0300

    uso de switch

---
+++ js_Basico_ao_Avancado/script/switch.js
@@ -0,0 +1,17 @@
+
+const valor = 8
+
+switch (valor) {
+    case 1:
+        console.log("Condição 1")
+        break
+    case 2:
+        console.log("Condição 2")
+        break
+    case 3:
+        console.log("Condição 3")
+        break
+    default:
+        console.log("Condição padrão")
+        break
+}
