c528b3bb5d098e4c5f4d591b40bd6d224b2df595
SwthSawTheme <sawtheme1@gmail.com>
2025-02-02 15:14:05 -0300

    Uso de função usando objeto (arguments)

--- js_Basico_ao_Avancado/script/funcs.js
+++ js_Basico_ao_Avancado/script/funcs.js
@@ -15,3 +15,19 @@ function sum(n1,n2) {
 var resultado = sum(101, 102)
 
 console.log(resultado)
+
+// retorno com varios argumentos
+
+function sumBase() {
+    let res = 0
+
+    for (const arg of arguments){
+        res += arg
+    }
+    return res
+}
+
+var teste = sumBase(4,22,4,5,55,6,7,8,9,99,546)
+var teste1 = sumBase()
+console.log(teste)
+console.log(teste1)
