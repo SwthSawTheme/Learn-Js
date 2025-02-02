45cc628eb0670c6a0242e40388de46cacaa94612
SwthSawTheme <sawtheme1@gmail.com>
2025-02-02 17:08:56 -0300

    Metodos para manipulação de string

--- js_Basico_ao_Avancado/script/manip_Strings.js
+++ js_Basico_ao_Avancado/script/manip_Strings.js
@@ -2,4 +2,15 @@
 
 var frase = "A rosa morta pelo vento"
 
+// split() metodo para separar as strings por meio de um caracter
+// join() metodo de concatenar as strings por meio de um caracter
 console.log(frase.split(" ").join(";"))
+
+// Toda a frase em maiuscula
+console.log(frase.toLocaleLowerCase())
+
+// Toda a frase em minuscula
+console.log(frase.toLocaleUpperCase())
+
+// Metodo para achar palavra em frase, retorna true ou false
+console.log(frase.includes("A"))
