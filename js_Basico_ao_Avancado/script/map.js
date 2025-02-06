e6ccefae979a136172d82711f963c91d9e373fa9
SwthSawTheme <sawtheme1@gmail.com>
2025-02-06 19:15:08 -0300

    Uso da função map, e utilização da função map pra diferentes aplicações em arrays

---
+++ js_Basico_ao_Avancado/script/map.js
@@ -0,0 +1,17 @@
+
+
+let array = [1,2,3,4,5,6,7,8]
+
+const double = array.map((n) => n * 2)
+
+console.log(double)
+
+var people = [
+    {name: "clary",age:32},
+    {name: "Saw", age:22},
+    {name: "Mex", age:54}
+]
+
+const peopleNomarlaized = people.map((n) => n.name.toLocaleUpperCase())
+
+console.log(peopleNomarlaized)
